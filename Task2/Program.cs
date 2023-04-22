474ba71ca6cd3f53eeac884f8b167fdfb690a941
KseniaKuzmina <ksenya.kuzmina83@gmail.com>
2023-04-22 12:39:14 +0300

    Tasks added

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int M = 1;
+int N = 5;
+
+
+
+int Sum(int M, int N)
+{
+    // int sum = M + N;
+
+    // if (N > M)
+    // {
+    //     sum = sum + Sum(M + 1, N - 1);
+    // }
+
+    // return sum;
+    if(M==N) return N;
+    else
+    return N+Sum(M,N-1);
+}
+System.Console.WriteLine(Sum(M, N));
---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int Akkerman(int n, int m)
+{
+    if (n==0)
+    return m+1;
+    else
+    if((n!=0) && ( m==0))
+    return Akkerman(n-1,1);
+    else
+    return Akkerman(n-1, Akkerman(n,m-1));
+}
+
+int n=3;
+int m=2;
+System.Console.WriteLine(Akkerman(n,m));
