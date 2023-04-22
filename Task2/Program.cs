1899a6f07bbce803e326b84292b10199789bf661
KseniaKuzmina <ksenya.kuzmina83@gmail.com>
2023-04-22 12:42:04 +0300

    Task 2 edit

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,16 +7,8 @@ int N = 5;
 
 int Sum(int M, int N)
 {
-    // int sum = M + N;
-
-    // if (N > M)
-    // {
-    //     sum = sum + Sum(M + 1, N - 1);
-    // }
-
-    // return sum;
-    if(M==N) return N;
+    if (M == N) return N;
     else
-    return N+Sum(M,N-1);
+        return N + Sum(M, N - 1);
 }
 System.Console.WriteLine(Sum(M, N));
